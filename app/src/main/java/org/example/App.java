4e4893f3885405bfb6d9c7a4eb59933fd7252811
Aahanaa Sharrma <sharrma24aahanaa@gmail.com>
2025-05-08 10:21:14 +0530

    feat: new commit

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -5,7 +5,7 @@ package org.example;
 
 public class App {
     public String getGreeting() {
-        return "Hello Jenkins!";
+        return "Hello EC2!";
     }
 
     public static void main(String[] args) {
